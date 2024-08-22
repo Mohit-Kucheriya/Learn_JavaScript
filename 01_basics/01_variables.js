d564432c391ea01f1f84342c316f835fdbc9e5e4
Mohit Kucheriya <112751474+Mohit-Kucheriya@users.noreply.github.com>
2024-08-22 05:33:41 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let accountName  = "Mohit Kucheriya";
+var accountEmail = "mohit@google.com";
+let accountState;
+
+accountName = "Kucheriya Mohit";
+accountEmail = "kucheriya@google.com";
+
+console.table([accountId,accountName,accountEmail,accountState]);
+
+// Notes:
+// we can declare the variable, simply assigning the memory for that variable before initialization the value to it.
+// re-initialize to constant are not allowed.
+// prefer not to use var because of block scope and functional scope.
+
